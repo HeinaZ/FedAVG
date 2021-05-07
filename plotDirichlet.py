58c29a44633f82b1ddfac937bcf81be601305dde
HeinaZ <38244207+HeinaZ@users.noreply.github.com>
2021-05-07 13:36:48 +0800

    Update plotDirichlet.py

--- plotDirichlet.py
+++ plotDirichlet.py
@@ -2,18 +2,25 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import time
+import math
 
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
 mpl.rcParams["axes.unicode_minus"] = False
 
-isIID = 0.06
+alpha = 0.01
 num_of_clients = 100
 shard_size = 300
 
-dirichlet_pdf = np.random.dirichlet([isIID/10] * 10, num_of_clients)
 actual_pdf = [0.1] * 10
+dirichlet_pdf = np.random.dirichlet([alpha / 10] * 10, num_of_clients)
+
+shared_portion = math.exp(-alpha)
+
+shared_size = shard_size*shared_portion/4
+shard_size = shard_size*(1 - shared_portion/4)
+
 for i in range(num_of_clients):
-    local_pdf = np.floor(dirichlet_pdf[i] * 300).astype('int64')
+    local_pdf = np.floor(dirichlet_pdf[i] * shard_size + np.multiply(actual_pdf, shared_size)).astype('int64')
     pdf_list = []
     start_index = 0
     for k in range(10):
@@ -24,6 +31,6 @@ for i in range(num_of_clients):
                 (i*4, 3),
                 facecolors=("b", "#FF7F50", "g", "r", "purple", "#8B4513", "#FFC0CB", "#808080", "#FFD700", "#00FFFF"),
                 alpha=1)
-plt.xlim((0, 295))
+plt.xlim((0, shard_size+shared_size-5))
 plt.axis('off')
-plt.savefig("./result/alpha_" + format(isIID) + '-' + format(time.time()) + ".png")
+plt.savefig("./result/alpha_" + format(alpha) + '-' + format(time.time()) + ".png")
